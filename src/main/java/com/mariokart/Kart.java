b596455530361fa8d3fb587b5b7356abf54c9408
Eric Schwartz <erics273@gmail.com>
2025-11-03 14:08:56 -0500

    First commit but also added the interface and started building the Kart abstract class

---
+++ src/main/java/com/mariokart/Kart.java
@@ -0,0 +1,30 @@
+package com.mariokart;
+
+public abstract class Kart implements Valuable{
+
+    //create some properties that describe a kart
+    private String name;
+    private double basePrice;
+
+    public Kart(String name, double basePrice) {
+        this.name = name;
+        this.basePrice = basePrice;
+    }
+
+    //getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+}
---
+++ src/main/java/com/mariokart/Valuable.java
@@ -0,0 +1,11 @@
+package com.mariokart;
+
+/*
+the interface will use for anything worth any money to us
+ */
+public interface Valuable {
+
+    //anything that implements this interface BETTER override the getValue mothod OR ELSE
+    public double getValue();
+
+}
