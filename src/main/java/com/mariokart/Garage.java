c8637a11437fbdca85a81a7febe17046b17cf15e
Eric Schwartz <erics273@gmail.com>
2025-11-03 15:05:53 -0500

    added the garage to hold all our valuable

---
+++ src/main/java/com/mariokart/Garage.java
@@ -0,0 +1,29 @@
+package com.mariokart;
+
+import java.util.ArrayList;
+
+public class Garage {
+
+    //the list of all the things in my garage that are worth money (that are Valuable)
+    private ArrayList<Valuable> valuablesInTheGarage;
+
+    public Garage() {
+        this.valuablesInTheGarage = new ArrayList<Valuable>();
+    }
+
+    public void add(Valuable theValuableToAdd){
+        valuablesInTheGarage.add(theValuableToAdd);
+    }
+
+    public double getTotalValueOfGarageValuables(){
+        //start our total as 0
+        double total = 0;
+
+        for(Valuable theCurrentValuable : valuablesInTheGarage){
+            total += theCurrentValuable.getValue();
+        }
+
+        return total;
+    }
+
+}
